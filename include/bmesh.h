576415c7837e5f7174568f16a4ee97735784b33c
NewXtSI <leerzeichen32@googlemail.com>
2023-07-29 14:24:36 +0200

    more functions

--- include/bmesh.h
+++ include/bmesh.h
@@ -4,28 +4,33 @@
 #include <Arduino.h>
 
 class BMesh {
+ protected:
+    using       BoolCallBack = void (*)(bool);
+    using       StringCallBack = void (*)(String);
+
+    StringCallBack  actionCallBack = nullptr;
  public:
                 BMesh();
-    bool        init(String meshName, String meshPassword, uint16_t meshPort);
-    void        setGatewayMode(String SSID, String Password, String MQTTServer, uint32_t MQTTPort, String MQTTUser, String MQTTPassword);
+    bool        init(String meshName, String meshPassword, uint16_t meshPort, uint8_t meshChannel);
+    void        setGatewayMode(String SSID, String Password);
     void        loop();
 
     bool        isGateway();
+    IPAddress   getlocalIP();           // Bei Gateway, STATION IP
 
     bool        isMeshConnected();      // Minimum 1 node can be reached
     bool        isGatewayConnected();   // For nodes: Gateway is set and can be reached
+
+    void        onAction(StringCallBack callBack);
  private:
     String      m_meshName;
     String      m_meshPassword;
     uint32_t    m_meshPort;
-
+    uint8_t     m_meshChannel;
+    IPAddress   m_IP;
     bool        m_isGateway;
     String      m_SSID;
     String      m_Password;
-    String      m_MQTTServer;
-    uint32_t    m_MQTTPort;
-    String      m_MQTTUser;
-    String      m_MQTTPassword;
 };
 
 #endif // __BMESH_H__
--- src/bmesh.cpp
+++ src/bmesh.cpp
@@ -1,45 +1,58 @@
 #include "../include/bmesh.h"
 
+#include <painlessMesh.h>
+#include <PubSubClient.h>
+
 ///////////////////////////////////////////////////////
 // Node featurelist:
 ///////////////////////////////////////////////////////
 // index    name                    implemented
 
+painlessMesh  __mesh;
+
+
 BMesh::BMesh() {
-
-}
-
-bool
-BMesh::init(String meshName, String meshPassword, uint16_t meshPort) {
-    bool bRet = true;
     m_meshName = m_meshName;
     m_meshPassword = meshPassword;
     m_meshPort = meshPort;
-
+    m_meshChannel = meshChannel;
     m_isGateway = false;
     m_SSID = "";
     m_Password = "";
-    m_MQTTServer = "";
-    m_MQTTPort = 0;
-    m_MQTTUser = "";
-    m_MQTTPassword = "";
+    m_IP = IPAddress(0, 0 , 0, 0);
+}
+
+bool
+BMesh::init(String meshName, String meshPassword, uint16_t meshPort, uint8_t meshChannel) {
+    bool bRet = true;
+    __mesh.setDebugMsgTypes(ERROR | STARTUP | CONNECTION | GENERAL | APPLICATION | DEBUG | MSG_TYPES);  // set before init() so that you can see startup messages
+    __mesh.init(m_meshName, m_meshPassword, m_meshPort, WIFI_AP_STA, m_meshChannel);
+    __mesh.setContainsRoot(true);
     return bRet;
 }
 
 void
-BMesh::setGatewayMode(String SSID, String Password, String MQTTServer,
-        uint32_t MQTTPort, String MQTTUser, String MQTTPassword) {
+BMesh::setGatewayMode(String SSID, String Password) {
     m_isGateway = true;
+    m_SSID = SSID;
+    m_Password = Password;
+
+    __mesh.stationManual(m_SSID, m_Password);
+    __mesh.setRoot(true);
 }
 
 void
 BMesh::loop() {
-
+    __mesh.update();
 }
 
 bool
 BMesh::isGateway() {
     return m_isGateway;
+}
+IPAddress
+BMesh::getlocalIP() {
+    return IPAddress(__mesh.getStationIP());
 }
 
 bool
@@ -49,8 +62,12 @@ BMesh::isMeshConnected() {
 }
 
 bool
-BMesh::isGatewayConnected()
- {
+BMesh::isGatewayConnected() {
     bool bRet = false;
     return bRet;
+}
+
+void
+BMesh::onAction(StringCallBack callBack) {
+    this->actionCallBack = callBack;
 }
