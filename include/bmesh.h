69020728679d32df5453ac78598296998647aa1e
NewXtSI <leerzeichen32@googlemail.com>
2023-07-29 13:15:05 +0200

    work

--- include/bmesh.h
+++ include/bmesh.h
@@ -1,9 +1,31 @@
 #ifndef __BMESH_H__
 #define __BMESH_H__
 
+#include <Arduino.h>
+
 class BMesh {
  public:
-                BMesh();    
+                BMesh();
+    bool        init(String meshName, String meshPassword, uint16_t meshPort);
+    void        setGatewayMode(String SSID, String Password, String MQTTServer, uint32_t MQTTPort, String MQTTUser, String MQTTPassword);
+    void        loop();
+
+    bool        isGateway();
+
+    bool        isMeshConnected();      // Minimum 1 node can be reached
+    bool        isGatewayConnected();   // For nodes: Gateway is set and can be reached
+ private:
+    String      m_meshName;
+    String      m_meshPassword;
+    uint32_t    m_meshPort;
+
+    bool        m_isGateway;
+    String      m_SSID;
+    String      m_Password;
+    String      m_MQTTServer;
+    uint32_t    m_MQTTPort;
+    String      m_MQTTUser;
+    String      m_MQTTPassword;
 };
 
 #endif // __BMESH_H__
--- src/bmesh.cpp
+++ src/bmesh.cpp
@@ -1,5 +1,56 @@
 #include "../include/bmesh.h"
 
+///////////////////////////////////////////////////////
+// Node featurelist:
+///////////////////////////////////////////////////////
+// index    name                    implemented
+
 BMesh::BMesh() {
 
+}
+
+bool
+BMesh::init(String meshName, String meshPassword, uint16_t meshPort) {
+    bool bRet = true;
+    m_meshName = m_meshName;
+    m_meshPassword = meshPassword;
+    m_meshPort = meshPort;
+
+    m_isGateway = false;
+    m_SSID = "";
+    m_Password = "";
+    m_MQTTServer = "";
+    m_MQTTPort = 0;
+    m_MQTTUser = "";
+    m_MQTTPassword = "";
+    return bRet;
+}
+
+void
+BMesh::setGatewayMode(String SSID, String Password, String MQTTServer,
+        uint32_t MQTTPort, String MQTTUser, String MQTTPassword) {
+    m_isGateway = true;
+}
+
+void
+BMesh::loop() {
+
+}
+
+bool
+BMesh::isGateway() {
+    return m_isGateway;
+}
+
+bool
+BMesh::isMeshConnected() {
+    bool bRet = false;
+    return bRet;
+}
+
+bool
+BMesh::isGatewayConnected()
+ {
+    bool bRet = false;
+    return bRet;
 }
